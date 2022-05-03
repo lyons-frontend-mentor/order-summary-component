05a0b6fff1de7a5fdb9f74a1858f18037d475b0a
Greg Lyons <gregory.lyons512@gmail.com>
2022-05-03 16:12:34 -0500

    Initial commit.

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        paleblue: "hsl(225, 100%, 94%)",
+        brightblue: "hsl(245, 75%, 52%)",
+        verypaleblue: "hsl(225, 100%, 98%)",
+        desatblue: "hsl(224, 23%, 55%)",
+        darkblue: "hsl(223, 47%, 23%)"
+      },
+      fontFamily: {
+        redhat: ["Red Hat Display", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
